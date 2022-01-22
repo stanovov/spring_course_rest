fe156487a5d50041cbc9078904e66b99d2e33eff
Semyon Stanovov <sestanovov@gmail.com>
2022-01-22 20:13:53 +0300

    Init commit

---
+++ README.md
@@ -0,0 +1,10 @@
+[![Build Status](https://app.travis-ci.com/stanovov/spring_course_rest.svg?branch=master)](https://app.travis-ci.com/stanovov/spring_course_rest)
+
+![](https://img.shields.io/badge/Maven-=_3-red)
+![](https://img.shields.io/badge/Java-=_17-orange)
+![](https://img.shields.io/badge/Spring-=_5-lightgreen)
+![](https://img.shields.io/badge/Checkstyle-lightgrey)
+
+# spring_course_rest
+
+Учебный репозиторий был создан для закрепления и повторения материала по Spring REST. Курс Заура Трегулова.
---
+++ src/main/java/ru/sestanovov/spring/rest/configuration/MyConfig.java
@@ -0,0 +1,55 @@
+package ru.sestanovov.spring.rest.configuration;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import javax.sql.DataSource;
+import java.beans.PropertyVetoException;
+import java.util.Properties;
+
+@Configuration
+@ComponentScan(basePackages = "ru.sestanovov.spring.rest")
+@EnableWebMvc
+@EnableTransactionManagement
+public class MyConfig {
+
+    @Bean
+    public DataSource dataSource() {
+        ComboPooledDataSource dataSource = new ComboPooledDataSource();
+        try {
+            dataSource.setDriverClass("com.mysql.cj.jdbc.Driver");
+            dataSource.setJdbcUrl("jdbc:mysql://localhost:3306/my_db?useSSL=false");
+            dataSource.setUser("bestuser");
+            dataSource.setPassword("bestuser");
+        } catch (PropertyVetoException e) {
+            e.printStackTrace();
+        }
+        return dataSource;
+    }
+
+    @Bean
+    public LocalSessionFactoryBean sessionFactoryBean() {
+        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+        sessionFactory.setDataSource(dataSource());
+        sessionFactory.setPackagesToScan("ru.sestanovov.spring.rest.entity");
+        Properties properties = new Properties();
+        properties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQLDialect");
+        properties.setProperty("hibernate.show_sql", "true");
+        sessionFactory.setHibernateProperties(properties);
+        return sessionFactory;
+    }
+
+    @Bean
+    public HibernateTransactionManager hibernateTransactionManager() {
+        HibernateTransactionManager transactionManager = new HibernateTransactionManager();
+        transactionManager.setSessionFactory(sessionFactoryBean().getObject());
+        return transactionManager;
+    }
+
+}
